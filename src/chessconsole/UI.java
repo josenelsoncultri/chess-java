3b1633ff0c85142a2a8674f3b493b94daf5513bf
José Nelson Cultri <josefcultrifrc@gmail.com>
2022-07-23 00:32:01 -0300

    Moving pieces

--- src/chessconsole/UI.java
+++ src/chessconsole/UI.java
@@ -1,10 +1,15 @@
 package chessconsole;
 
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
     static final int CHESS_BOARD_ROW_LENGTH = 8;
+
     // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
@@ -24,6 +29,18 @@ public class UI {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    public static ChessPosition readChessPosition(Scanner scanner) {
+        try {
+            String s = scanner.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+
+            return new ChessPosition(column, row);
+        } catch (RuntimeException ex) {
+            throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8");
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
